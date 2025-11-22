dc72a54de69e726eb7dda026e10091238a24f235
JAVAGONMUS <93622691+JAVAGONMUS@users.noreply.github.com>
2025-11-21 20:29:12 -0600

    Create Driver model using Sequelize

---
+++ config/database.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require('sequelize');
+const { sequelizePostgres } = require('../config/databases');
+// ... define el modelo Driver usando sequelizePostgres
+
+
+const Driver = sequelize.define('Driver', {
+  driver_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  user_id: { // Clave foránea a la tabla de Usuarios para el Login
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    unique: true
+  },
+  // Estado: 'disponible', 'no_disponible', 'en_viaje'
+  status: { 
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: 'no_disponible',
+  },
+  current_lat: {
+    type: DataTypes.DECIMAL(10, 8),
+    allowNull: true,
+  },
+  current_lng: {
+    type: DataTypes.DECIMAL(11, 8),
+    allowNull: true,
+  }
+}, {
+  tableName: 'drivers',
+  timestamps: false,
+});
+
+module.exports = Driver;
