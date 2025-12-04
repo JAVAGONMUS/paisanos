9f26d4faab11537c09f863f2968a6527730ee782
JAVAGONMUS <93622691+JAVAGONMUS@users.noreply.github.com>
2025-12-04 13:57:27 -0600

    Fix Municipio model import and association setup

--- config/associations.js
+++ config/associations.js
@@ -2,19 +2,19 @@
 
 const Pais = require('../models/Pais');
 const Departamento = require('../models/Departamento');
-const Municipio = require = ('../models/Municipio');
+const Municipio = require('../models/Municipio'); 
 
 exports.setupAssociations = () => {
     // 1. Relación entre PAISES y DEPARTAMENTOS
     Pais.hasMany(Departamento, {
-        foreignKey: 'ID_PAIS', // Campo en DEPARTAMENTOS que referencia a PAISES
+        foreignKey: 'ID_PAIS', 
         as: 'departamentos'
     });
     Departamento.belongsTo(Pais, { foreignKey: 'ID_PAIS' });
 
     // 2. Relación entre DEPARTAMENTOS y MUNICIPIOS
-    Departamento.hasMany(Municipio, {
-        foreignKey: 'ID_DEP', // Campo en MUNICIPIOS que referencia a DEPARTAMENTOS
+    Departamento.hasMany(Municipio, { // Esta línea es la que fallaba
+        foreignKey: 'ID_DEP', 
         as: 'municipios'
     });
     Municipio.belongsTo(Departamento, { foreignKey: 'ID_DEP' });
