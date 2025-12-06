2b3251acafbae732f49bddda1d7f875ecaddd81a
JAVAGONMUS <93622691+JAVAGONMUS@users.noreply.github.com>
2025-12-06 00:51:03 -0600

    Update catalogsController.js

--- controllers/catalogsController.js
+++ controllers/catalogsController.js
@@ -1,26 +1,16 @@
-// controllers/catalogsController.js
 const Dominio = require('../models/Dominio');
 
-/**
- * Obtiene la lista de dominios de correo electrónico disponibles 
- * desde la tabla DOMINIOS y los formatea para ser utilizados en un select (label/value).
- * @param {object} req - Objeto de solicitud de Express.
- * @param {object} res - Objeto de respuesta de Express.
- */
 exports.getDomains = async (req, res) => {
     try {
         // 1. Buscar todos los dominios
         const dominios = await Dominio.findAll({
-            
             attributes: ['ID_DOM', 'NOMBRE'], 
-            // Opcional: ordenar por nombre para mejor usabilidad
-            order: [['NOMBRE_DOMINIO', 'ASC']] // Usar el nombre de la propiedad del modelo para ordenar
+            order: [['NOMBRE_DOMINIO', 'ASC']] 
         });
         console.log('DOMINIOS ENCONTRADOS:', dominios.length); 
         
         // 2. Formatear los datos a { label: 'nombre', value: 'nombre' }
         const formattedDomains = dominios.map(d => {
-            
             const domainName = d.NOMBRE_DOMINIO; 
             
             return {
