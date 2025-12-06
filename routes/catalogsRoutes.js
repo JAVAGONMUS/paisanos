18ff1cb05639182aed03111ee5603225a8f7fbc0
JAVAGONMUS <93622691+JAVAGONMUS@users.noreply.github.com>
2025-12-05 21:08:14 -0600

    Update catalogsRoutes.js

--- routes/catalogsRoutes.js
+++ routes/catalogsRoutes.js
@@ -4,9 +4,6 @@ const router = express.Router();
 // Importa el controlador de catálogos
 const catalogsController = require('../controllers/catalogsController');
 
-// Ruta para obtener el catálogo de dominios
-// La ruta que uses aquí (por ejemplo, '/domains') se combinará con el prefijo
-// que le des en app.js/index.js (ej: /api/catalogs/domains)
 router.get('/domains', catalogsController.getDomains);
 
 module.exports = router;
