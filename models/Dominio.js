40e756b78709a84852b850109b9111d3a744b17e
JAVAGONMUS <93622691+JAVAGONMUS@users.noreply.github.com>
2025-12-05 19:46:14 -0600

    Rename NOMBRE field to NOMBRE_DOMINIO in Dominio model
    Updated the NOMBRE field to NOMBRE_DOMINIO for clarity.

--- models/Dominio.js
+++ models/Dominio.js
@@ -1,18 +1,18 @@
-// /app/models/Dominio.js
 const { DataTypes } = require('sequelize');
 const { sequelizeMySQL } = require('../config/databases'); 
 
 const Dominio = sequelizeMySQL.define('Dominio', {
+    // Llave primaria
     ID_DOM: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true,
         field: 'ID_DOM',
     },
-    NOMBRE: {
+    NOMBRE_DOMINIO: {
         type: DataTypes.STRING,
         allowNull: false,
-        field: 'NOMBRE',
+        field: 'NOMBRE', 
     },
 }, {
     tableName: 'DOMINIOS', 
