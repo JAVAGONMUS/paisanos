d3c0aa4a4f53401028a3f37c62b1a2b8d4156b66
JAVAGONMUS <93622691+JAVAGONMUS@users.noreply.github.com>
2025-12-06 02:03:20 -0600

    Update catalogsController.js

--- controllers/catalogsController.js
+++ controllers/catalogsController.js
@@ -6,7 +6,6 @@ exports.getDomains = async (req, res) => {
         const dominios = await Dominio.findAll({
             attributes: ['ID_DOM', 'NOMBRE'], 
         });
-        console.log('DOMINIOS ENCONTRADOS:', dominios.length);
         
         // 2. Formatear los datos a { label: 'nombre', value: 'nombre' }
         const formattedDomains = dominios.map(d => {
@@ -17,8 +16,6 @@ exports.getDomains = async (req, res) => {
             };
         });
         
-        console.log('DOMINIOS FORMATEADOS (Primeros 2):', formattedDomains.slice(0, 2)); 
-
         res.status(200).json(formattedDomains);
     } catch (error) {
         // 4. Manejar errores de base de datos o internos
