792ded6d1604910f5540f378ac36f5a8360b891e
JAVAGONMUS <93622691+JAVAGONMUS@users.noreply.github.com>
2025-12-31 07:01:06 -0600

    Add registration route for drivers

--- routes/driverRoutes.js
+++ routes/driverRoutes.js
@@ -5,15 +5,22 @@ const verifyToken = require('../middlewares/authMiddleware');
 const { getUbicaciones } = require('../controllers/ubicacionController');
 
 // --- RUTAS PÚBLICAS ---
+
+// Ruta para el inicio de sesión formal
 router.post('/login', driverController.loginDriver);
 
-// 💡 AÑADIR LA RUTA DE REGISTRO
-router.post('/register', driverController.registerDriver); // <-- ¡ESTA ES LA RUTA FALTANTE!
+// Ruta para el registro de nuevos conductores
+router.post('/register', driverController.registerDriver);
+
+router.get('/check-username/:username', driverController.checkUsername);
+
 
 // --- RUTAS PROTEGIDAS POR JWT ---
-router.post('/status', verifyToken, driverController.updateStatus);
-// router.get('/history', verifyToken, driverController.getTripHistory); 
 
+// Actualización de estado (disponible/no disponible)
+router.post('/status', verifyToken, driverController.updateStatus);
+
+// Obtener ubicaciones para el mapa
 router.get('/ubicaciones', getUbicaciones);
 
 module.exports = router;
