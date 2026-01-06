4d0103ce35caf82df90216604bcbf6fdc7a45123
JAVAGONMUS <93622691+JAVAGONMUS@users.noreply.github.com>
2026-01-06 01:38:24 -0600

    Add update location route with JWT verification
    Added a new route to update location with token verification.

--- routes/ubicacionRoutes.js
+++ routes/ubicacionRoutes.js
@@ -1,9 +1,11 @@
-// /app/routes/ubicacionRoutes.js
 const express = require('express');
 const router = express.Router();
 const ubicacionController = require('../controllers/ubicacionController');
+const { actualizarUbicacionConductor } = require('../controllers/ubicacionController');
+const { verificarToken } = require('../middlewares/authMiddleware'); // Tu seguridad JWT
 
-// Ruta para obtener el catálogo de departamentos con municipios anidados
 router.get('/ubicaciones', ubicacionController.getUbicaciones);
+
+router.post('/update-location', verificarToken, actualizarUbicacionConductor);
 
 module.exports = router;
