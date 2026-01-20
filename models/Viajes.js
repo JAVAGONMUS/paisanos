7f130f3429fa361eba620d67bdcd7ace315d8571
JAVAGONMUS <93622691+JAVAGONMUS@users.noreply.github.com>
2026-01-19 21:55:18 -0600

    Add files via upload

---
+++ models/Viajes.js
@@ -0,0 +1,14 @@
+const { DataTypes } = require('sequelize');
+const { sequelizePostgres } = require('../config/databases');
+
+const Viajes = sequelizePostgres.define('Viajes', {
+    ID_VIAJE: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    ID_SOL: { type: DataTypes.INTEGER, allowNull: false },
+    STATUS: { 
+            type: DataTypes.ENUM('PENDIENTE', 'ACEPTADA', 'ACTIVA', 'COMPLETADA', 'CANCELADA'),
+            defaultValue: 'PENDIENTE' 
+        },
+    TARIFA: { type: DataTypes.DECIMAL(8, 2), allowNull: false }
+}, { tableName: 'VIAJES_ACTIVOS', timestamps: false });
+
+module.exports = Viajes;
