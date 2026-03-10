9c786af40b9441b6f058fcb4d9185822a82158ae
JAVAGONMUS <93622691+JAVAGONMUS@users.noreply.github.com>
2026-03-10 13:08:28 -0600

    Update ubicacionRoutes.js

--- routes/ubicacionRoutes.js
+++ routes/ubicacionRoutes.js
@@ -3,12 +3,11 @@ const router = express.Router();
 // Importación limpia de ambas funciones desde el controlador
 const { getUbicaciones, actualizarUbicacionConductor } = require('../controllers/ubicacionController');
 // Corregido: Debe ser verifyToken para coincidir con tu authMiddleware.js
-const verifyToken = require('../middlewares/authMiddleware'); 
-
+const verifyToken = require('../middlewares/authMiddleware');
+const ubicacionController = require('../controllers/ubicacionController');
 // Obtener catálogo de departamentos/municipios
 router.get('/ubicaciones', getUbicaciones);
-
 // Actualizar ubicación actual (Ruta protegida para el GPS)
 router.post('/update-location', verifyToken, actualizarUbicacionConductor);
-
+router.get('/zona-activa', verifyToken, ubicacionController.obtenerZonaPorUbicacion);
 module.exports = router;
