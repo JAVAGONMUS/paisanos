314c1d36b9e9730210c6a9d4d53431c1e814ffe6
JAVAGONMUS <93622691+JAVAGONMUS@users.noreply.github.com>
2026-03-24 20:33:31 -0600

    Update ubicacionRoutes.js

--- routes/ubicacionRoutes.js
+++ routes/ubicacionRoutes.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const ubicacionController = require('../controllers/ubicacionController');
-const verifyToken = require('../middlewares/authMiddleware');
+let verifyToken = require('../middlewares/authMiddleware');
 
-// Obtener departamentos y municipios
+// Auto-corrección
+if (typeof verifyToken === 'object' && verifyToken.verifyToken) verifyToken = verifyToken.verifyToken;
+
+// 1. Obtener departamentos y municipios
 router.get('/ubicaciones', verifyToken, ubicacionController.getUbicaciones);
 
-// Actualizar GPS crudo en Postgres
+// 2. Actualizar GPS crudo en Postgres
 router.post('/actualizar', verifyToken, ubicacionController.actualizarUbicacionConductor);
 
-// Punto 11: Obtener la Geocerca Dinámica según el GPS del conductor
+// 3. Punto 11: Obtener la Geocerca Dinámica según el GPS del conductor
 router.get('/zona-activa', verifyToken, ubicacionController.obtenerZonaPorUbicacion);
 
 module.exports = router;
